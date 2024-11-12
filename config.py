9bf6d9870644964325acb7bb5219ef33392a3522
CoderRzps <ayushpritysingh098@gmail.com>
2024-11-12 17:35:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shortxlinks.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "fa2a0768fc8d2a51b22e46293634a52670a73c7a")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_download_tutorial_idk/2")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/APSmo/8")
 
 
 #force sub channel id, if you want enable force sub
