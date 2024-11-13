eb411bdf47cacae472e0cabebe3344bbb6bfb3bc
CoderRzps <ayushpritysingh098@gmail.com>
2024-11-13 22:32:20 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -6,22 +6,11 @@ database = dbclient[DB_NAME]
 
 user_data = database['users']
 
-default_verify = {
-    'is_verified': False,
-    'verified_time': 0,
-    'verify_token': "",
-    'link': ""
-}
-
+# Default user data without verification status
 def new_user(id):
     return {
         '_id': id,
-        'verify_status': {
-            'is_verified': False,
-            'verified_time': "",
-            'verify_token': "",
-            'link': ""
-        }
+        # Remove the verify_status part
     }
 
 async def present_user(user_id: int):
@@ -33,14 +22,15 @@ async def add_user(user_id: int):
     await user_data.insert_one(user)
     return
 
-async def db_verify_status(user_id):
-    user = await user_data.find_one({'_id': user_id})
-    if user:
-        return user.get('verify_status', default_verify)
-    return default_verify
+# Remove verification status-related code
+# async def db_verify_status(user_id):
+#     user = await user_data.find_one({'_id': user_id})
+#     if user:
+#         return user.get('verify_status', default_verify)
+#     return default_verify
 
-async def db_update_verify_status(user_id, verify):
-    await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
+# async def db_update_verify_status(user_id, verify):
+#     await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
 
 async def full_userbase():
     user_docs = user_data.find()
