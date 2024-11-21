41684946546c44b254e88188351a565cadc90ad2
Inder717 <bet.nil2024@gmail.com>
2024-11-21 22:15:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ TUT_VID = os.environ.get("https://t.me/tokenbotuse/6")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002424794894"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002424794894"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
